b13b9cacb8731e8591d862bafe72db4d5889b526
Sherry Wang <feixue1121@163.com>
2021-10-04 14:49:15 +0800

    bandwidth QoS support

--- socket_sim/src/socket_sim_client.cc
+++ socket_sim/src/socket_sim_client.cc
@@ -1,6 +1,119 @@
-#include <iostream>
 
-int main()
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <chrono>
+#include <atomic>
+#include <thread>
+#include <mutex>
+
+#define PORT 8080
+#define LEN 2048
+
+std::mutex g_pkts_mtx;
+int64_t g_pkts = 0;
+
+void send_loop()
 {
+  int sock = 0;
+  struct sockaddr_in serv_addr;
+  char buffer[LEN] = {'a'};
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    std::cout << "Socket creation error, "
+	      << strerror(errno)
+              << std::endl;
+    return; 
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  // Convert IPv4 and IPv6 addresses from text to binary form
+  if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+  {
+    std::cout << "Invalid address/ Address not supported, "
+	      << strerror(errno)
+              << std::endl;
+    return;
+  }
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  {
+    std::cout << "Connection Failed, "
+	      << strerror(errno)
+              << std::endl;;
+    return;
+  }
+
+  int buff_size = (16 * 1024 * 1024);
+  setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &buff_size, sizeof(buff_size)); 
+
+  while (true)
+  {
+    int ret = send(sock, buffer, LEN, 0 );
+    if (ret < 0)
+    {
+      std::cout << "ret = "
+		<< ret
+		<< ", "
+		<< strerror(errno)
+		<< std::endl;
+      break;
+    }
+    if (ret != LEN)
+    {
+      std::cout << "ret = "
+		<< ret
+		<< std::endl;
+    }
+    {
+      std::lock_guard<std::mutex> lock(g_pkts_mtx);
+      g_pkts++;
+    }
+  }
+}
+
+void print_statistics()
+{
+  while (true)
+  {
+    int64_t start = std::chrono::duration_cast<std::chrono::seconds>(
+			    std::chrono::system_clock::now().time_since_epoch()).count();
+
+    sleep(3);
+
+    int64_t end = std::chrono::duration_cast<std::chrono::seconds>(
+			    std::chrono::system_clock::now().time_since_epoch()).count();
+
+    int64_t pkts = 0;
+    {
+      std::lock_guard<std::mutex> lock(g_pkts_mtx);
+      pkts = g_pkts;
+      g_pkts = 0;
+    }
+    std::cout << "start = "
+	      << start
+	      << ", end = "
+	      << end 
+	      << ", qps = "
+	      << pkts / (end - start)
+	      << "/s, bandwidth = "
+	      << (pkts * LEN) / 1024 / 1024 / (end - start)
+	      << " MiB/s"
+	      << std::endl;
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  std::thread thd = std::thread(print_statistics);
+  send_loop();
+  thd.join();
   return 0;
 }
--- socket_sim/src/socket_sim_server.cc
+++ socket_sim/src/socket_sim_server.cc
@@ -1,6 +1,147 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 #include <iostream>
+#include <chrono>
+#include <atomic>
+#include <thread>
+#include <mutex>
 
-int main()
+#define PORT 8080
+#define LEN  2048
+
+std::mutex g_pkts_mtx;
+int64_t g_pkts = 0;
+
+void receive_loop()
 {
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[LEN];
+  
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+  {
+    std::cout << "socket failed, "
+	      << strerror(errno)
+	      << std::endl;
+    return;
+  }
+  
+  // Forcefully attaching socket to the port 8080
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+		 &opt, sizeof(opt)))
+  {
+    std::cout << "setsockopt, "
+	      << strerror(errno)
+	      << std::endl;
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+  
+  // Forcefully attaching socket to the port 8080
+  if (bind(server_fd, (struct sockaddr *)&address,
+	   sizeof(address)) < 0)
+  {
+    std::cout << "bind failed, "
+	      << strerror(errno)
+	      << std::endl;
+  }
+  if (listen(server_fd, 3) < 0)
+  {
+    std::cout << "listen, "
+	      << strerror(errno)
+	      << std::endl;
+    return;
+  }
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+			   (socklen_t*)&addrlen)) < 0)
+  {
+    std::cout << "accept, "
+	      << strerror(errno)
+	      << std::endl;
+    return;
+  }
+
+  int buff_size = (16 * 1024 * 1024);
+  setsockopt(new_socket, SOL_SOCKET, SO_RCVBUF, &buff_size, sizeof(buff_size));
+
+  while (true)
+  {
+    int len = 0, ret = 0;
+    while (len != LEN)
+    {
+      ret = read(new_socket, buffer, LEN);
+      if (ret < 0)
+      {
+	std::cout << "ret = "
+		  << ret
+		  << ", "
+		  << strerror(errno)
+		  << std::endl;
+	break;
+      }
+      len += ret;
+    }
+    if (len != LEN)
+    {
+      std::cout << "ret = "
+		<< ret
+		<< std::endl;
+      break;
+    }
+    {
+      std::lock_guard<std::mutex> lock(g_pkts_mtx);
+      g_pkts++;
+    }
+  }
+}
+
+void print_statistics()
+{
+  while (true)
+  {
+    int64_t start = std::chrono::duration_cast<std::chrono::seconds>(
+			    std::chrono::system_clock::now().time_since_epoch()).count();
+
+    sleep(3);
+
+    int64_t end = std::chrono::duration_cast<std::chrono::seconds>(
+			    std::chrono::system_clock::now().time_since_epoch()).count();
+
+    int64_t pkts = 0;
+    {
+      std::lock_guard<std::mutex> lock(g_pkts_mtx);
+      pkts = g_pkts;
+      g_pkts = 0;
+    }
+    std::cout << "start = "
+	      << start
+	      << ", end = "
+	      << end 
+	      << ", qps = "
+	      << pkts / (end - start)
+	      << "/s, bandwidth = "
+	      << (pkts * LEN) / 1024 / 1024 / (end - start)
+	      << " MiB/s"
+	      << std::endl;
+  }
+}
+
+
+int main(int argc, char const *argv[])
+{
+  std::thread thd = std::thread(print_statistics);
+  receive_loop();
+  
+  thd.join();
   return 0;
 }
+
